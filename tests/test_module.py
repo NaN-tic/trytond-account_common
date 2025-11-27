9bf98c4059eaeaf89514861a486c694970d64923
Bernat Brunet <bernat@nan-tic.com>
2025-11-27 17:17:23 +0100

    first commit

---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part account_common module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class AccountCommonTestCase(ModuleTestCase):
+    'Test Account Common module'
+    module = 'account_common'
+
+del ModuleTestCase
